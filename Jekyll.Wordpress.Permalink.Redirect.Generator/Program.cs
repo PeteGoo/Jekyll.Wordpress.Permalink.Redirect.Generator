1c4aa3ea93b2f4b9b4b93103d3eaac41ebb4f839
PeteGoo <pete@petegoo.com>
2014-04-26 23:44:25 +1200

    Initial commit

---
+++ Jekyll.Wordpress.Permalink.Redirect.Generator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Jekyll.Wordpress.Permalink.Redirect.Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || !args.Any())
+            {
+                throw new ArgumentNullException("args");
+            }
+            var directory = args[0];
+
+            if (!Directory.Exists(directory))
+            {
+                throw new ArgumentException(string.Format("The directory '{0}' does not exist", directory), "args");
+            }
+
+            var files = Directory.GetFiles(directory, "*.html");
+
+            Console.WriteLine(string.Format("Found {0} html files.", files.Count()));
+
+            if (!files.Any())
+            {
+                return;
+            }
+
+            Console.WriteLine("Press any key to start processing files");
+            Console.ReadKey(true);
+
+            Array.ForEach(files, AddRedirect);
+        }
+
+        private static readonly Regex PostNameRegex = new Regex(@"(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})-(?<slug>.*)");
+
+        private static void AddRedirect(string filePath)
+        {
+            var postName = Path.GetFileNameWithoutExtension(filePath);
+
+            var match = PostNameRegex.Match(postName);
+            var year = match.Groups["year"].Value;
+            var month = match.Groups["month"].Value;
+            var day = match.Groups["day"].Value;
+            var slug = match.Groups["slug"].Value;
+
+            var redirect = string.Format("/index.php/{0}/{1}/{2}/{3}/", year, month, day, slug);
+
+            Encoding encoding;
+            string contents;
+            using (var reader = new StreamReader(filePath))
+            {
+                contents = reader.ReadToEnd();
+                encoding = reader.CurrentEncoding;
+            }
+
+            contents = contents.Replace("published: true", string.Format("redirect_from: {0}\r\npublished: true", redirect));
+
+            File.WriteAllText(filePath, contents, new UTF8Encoding(false));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 Jekyll.Wordpress.Permalink.Redirect.Generator
 =============================================
 
-Generates redirect_from into Jekyll posts imported from Wordpress so that index.php/2014/04/15/foo-bar works
+If you import posts from Wordpress into your jekyll site you may need to redirect requests to `/index.php/2014/04/15/foo-bar` to your permalink format, in my case `/:year/:month/:day/:title/`. This assumes you can use the [jekyll-redirect-from gem](https://github.com/jekyll/jekyll-redirect-from) which is supported by GitHub pages.
+
+# Pre-Requisites
+
+* Make sure you have imported your posts using e.g. `jekyll import wordpressdotcom --source wordpress.xml`
+* Set the permalink style by adding the code below to your _config.yml
+```YAML
+permalink: /:year/:month/:day/:title/
+```
+* Make sure you are using the [jekyll-redirect-from gem](https://github.com/jekyll/jekyll-redirect-from) by adding the following to your _config.yml file
+```YAML
+gems:
+- jekyll-redirect-from
+```
+* Run `bundle` to install the necessary gem above
+* Run Jekyll.Wordpress.Permalink.Redirect.Generator specifying the _posts folder
+```
+Jekyll.Wordpress.Permalink.Redirect.Generator.exe c:\path\to\my\site\_posts
+```
+* Check the output
+
+
